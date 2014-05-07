b0ce814cebf7ed1aff6b3c8b6ef34e5ba923bf14
mziemer21 <mziemer@wisc.edu>
2014-05-07 00:35:48 -0500

    map button now brings up a map

--- src/com/example/dealio/MapActivity.java
+++ src/com/example/dealio/MapActivity.java
@@ -1,5 +1,16 @@
 package com.example.dealio;
 
-public class MapActivity {
+
+import android.os.Bundle;
+
+import com.example.dealio.navDrawer.NavDrawerTabs;
+
+public class MapActivity extends NavDrawerTabs {
+
+ @Override
+ protected void onCreate(Bundle savedInstanceState) {
+  super.onCreate(savedInstanceState);
+  setContentView(R.layout.activity_map);
+ }
 
 }
