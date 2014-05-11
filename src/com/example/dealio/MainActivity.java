7c2210c6d1ea3f689a96768f66aa22c9a919548a
Ryan Misura <misura@wisc.edu>
2014-05-10 22:12:34 -0500

    Added Background image of downtown, can change this to anything if desirable. Also changed onBackPressed() so it does not display empty MainActivity display

--- src/com/example/dealio/MainActivity.java
+++ src/com/example/dealio/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.dealio;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.dealio.navDrawer.NavDrawerActivity;
@@ -14,6 +15,13 @@ public class MainActivity extends NavDrawerActivity{
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+	}
+	
+	@Override
+	public void onBackPressed() {
+		Intent i = new Intent(getApplicationContext(), LoginActivity.class);
+		i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK); 
+		startActivity(i);
 	}
 	
 }
